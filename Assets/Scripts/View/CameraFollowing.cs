c89ff13b92674202a8b8caddfbeb6d80617d6c89
Lekantrop <98541354+Lekantrop-gd@users.noreply.github.com>
2023-10-27 18:51:01 +0300

    Add CameraFollowing skript

---
+++ Assets/Scripts/View/CameraFollowing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{
+    [SerializeField] private float _offset;
+    private float _followingSpeed;
+    private Transform _balloon;
+
+    public void Initialize(Transform balloon, float followingSpeed) 
+    {  
+        _followingSpeed = followingSpeed;
+        _balloon = balloon; 
+    }
+
+    public IEnumerator StartFolowing()
+    {
+        while (true)
+        {
+            transform.position = Vector3.MoveTowards(
+            transform.position,
+            new Vector3(_balloon.position.x, _balloon.position.y + _offset, transform.position.z),
+            _followingSpeed * Time.deltaTime
+            );
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
