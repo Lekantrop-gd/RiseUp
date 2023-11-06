c0d6d5d038d2e151e9b53f4501940e5ce55ddc71
Lekantrop <98541354+Lekantrop-gd@users.noreply.github.com>
2023-11-06 11:28:56 +0200

    Configure gravity

--- Assets/Scripts/View/General/Obstacle.cs
+++ Assets/Scripts/View/General/Obstacle.cs
@@ -1,9 +1,23 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Obstacle : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.isKinematic = true;
+    }
+
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _rigidbody.isKinematic = false;
     }
 }
